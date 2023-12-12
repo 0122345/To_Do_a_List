6a7ca889732bf4d34fe94fe8c8781e157cdb2e8a
0122345 <ntwarifiacre043@gmail.com>
2023-12-12 17:03:46 +0200

    styles

--- public/interface.js
+++ public/interface.js
@@ -3,3 +3,5 @@ const  rotatebtn = document.getElementById('rotatebtn');
 rotatebtn.addEventListener('click', function() {
     this.classList.toggle('rotated')
 });
+
+//const year = prompt("what is your name");
